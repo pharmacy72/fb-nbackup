c31393533a52c52a2cf56c3d6ae72c25d148b38c
Aleksey Arteev <alarteev@yandex.ru>
2020-05-11 23:05:00 +0500

    sql script: test data

--- scripts/create.db.sql
+++ scripts/create.db.sql
@@ -1,1 +1,25 @@
--- CREATE DATABASE 'NBEXAMPLE';
+connect NBEXAMPLE;
+
+CREATE TABLE TESTDATA (
+    ID INTEGER PRIMARY KEY NOT NULL,
+    NAME VARCHAR(200)
+);
+
+delete from TESTDATA;
+-- test data
+SET TERM ^ ;
+execute block
+as
+declare i int = 0;
+begin
+  while (i < 10000) do
+  begin
+    insert into TESTDATA values (:i, 'DATA-'||:i);
+    i = i + 1;
+  end
+end^
+SET TERM ; ^
+
+select count(1) from TESTDATA;
+
+commit;
--- scripts/init.example.sh
+++ scripts/init.example.sh
@@ -2,12 +2,15 @@
 FIREBIRD_PASSWORD="023RsdTf4UI123"
 FIREBIRD_USER="fbuser"
 FIREBIRD_DATABASE="NBEXAMPLE"
+FB_CONTAINER=firebird
 
-docker rm -f firebird
+docker rm -f ${FB_CONTAINER}
 docker run \
-	-v backup:/backup \
+	-v "${PWD}"/backup:/backup \
 	-e FIREBIRD_PASSWORD=$FIREBIRD_PASSWORD \
-       	-e FIREBIRD_USER=$FIREBIRD_USER\
+  -e FIREBIRD_USER=$FIREBIRD_USER\
 	-e FIREBIRD_DATABASE=$FIREBIRD_DATABASE\
-	--name firebird -d  jacobalberty/firebird:3.0
+	--name ${FB_CONTAINER} -d  jacobalberty/firebird:3.0
 
+ISQL="docker exec -i $FB_CONTAINER /usr/local/firebird/bin/isql -e -user ${FIREBIRD_USER} -password ${FIREBIRD_PASSWORD} -database ${FIREBIRD_DATABASE}"
+$ISQL < $(dirname "$0")/create.db.sql
