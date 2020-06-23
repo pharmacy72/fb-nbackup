c4dab84582a54051c20141ed782e2a61bf21dc58
Aleksey Arteev <alarteev@yandex.ru>
2020-06-24 01:09:34 +0500

    unit tests: manager commands

--- manager_test.go
+++ manager_test.go
@@ -1,6 +1,7 @@
 package fb_nbackup
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	mock_fb_nbackup "fb-nbackup/mock"
@@ -62,6 +63,137 @@ func TestParseArguments(t *testing.T) {
 			assert.EqualError(t, err, test.Err)
 		}
 	}
+}
+
+func TestManager_Common(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	executer := mock_fb_nbackup.NewMockexecuter(ctrl)
+
+	manager := NewManager(
+		WithCommandPath("cmd"),
+		withExecuter(executer))
+
+	db := "database.fdb"
+
+	commands := []struct {
+		name         string
+		baseCommand  string
+		method       func(context.Context) error
+		getArguments func(name string) []interface{}
+	}{
+		{
+			name:        "FIXUP",
+			baseCommand: "-FIXUP",
+			method: func(ctx context.Context) error {
+				return manager.Fixup(ctx, db)
+			},
+		},
+		{
+			name:        "UNLOCK",
+			baseCommand: "-UNLOCK",
+			method: func(ctx context.Context) error {
+				return manager.Unlock(ctx, db)
+			},
+		},
+		{
+			name:        "BACKUP",
+			baseCommand: "-BACKUP",
+			method: func(ctx context.Context) error {
+				return manager.Backup(ctx, NewLevel(2), db, "some file")
+			},
+			getArguments: func(name string) []interface{} {
+				return []interface{}{name, NewLevel(2).String(), db, "some file"}
+			},
+		},
+		{
+			name:        "RESTORE",
+			baseCommand: "-RESTORE",
+			method: func(ctx context.Context) error {
+				return manager.Restore(ctx, db, "file1", "fileA", "fileN")
+			},
+			getArguments: func(name string) []interface{} {
+				return []interface{}{name, db, "file1", "fileA", "fileN"}
+			},
+		},
+	}
+
+	for _, command := range commands {
+		t.Run(command.name, func(t *testing.T) {
+			getArgs := command.getArguments
+			if getArgs == nil {
+				getArgs = func(name string) []interface{} {
+					return []interface{}{name, db}
+				}
+			}
+
+			errWant := errors.New("error exec")
+			executer.EXPECT().Exec(ctx, "cmd", getArgs(command.baseCommand)...).Return(errWant)
+			err := command.method(ctx)
+			assert.EqualError(t, err, errWant.Error())
+
+			executer.EXPECT().Exec(ctx, "cmd", getArgs(command.baseCommand)...)
+			err = command.method(ctx)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestManager_BackupTo(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	executer := mock_fb_nbackup.NewMockexecuter(ctrl)
+
+	manager := NewManager(
+		WithCommandPath("cmd"),
+		withExecuter(executer))
+
+	db := "database.fdb"
+	level := NewLevel(2)
+	writer := &bytes.Buffer{}
+
+	errWant := errors.New("error exec")
+	executer.EXPECT().ExecWithWriter(ctx, "cmd", writer, "-BACKUP", level.String(), db, "stdout").
+		Return(errWant)
+	err := manager.BackupTo(ctx, level, db, writer)
+	assert.EqualError(t, err, errWant.Error())
+
+	executer.EXPECT().ExecWithWriter(ctx, "cmd", writer, "-BACKUP", level.String(), db, "stdout")
+	err = manager.BackupTo(ctx, level, db, writer)
+	assert.NoError(t, err)
+}
+
+func TestManager_Lock(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	executer := mock_fb_nbackup.NewMockexecuter(ctrl)
+
+	manager := NewManager(
+		WithCommandPath("cmd"),
+		withExecuter(executer))
+
+	db := "database.fdb"
+
+	errWant := errors.New("error exec")
+	executer.EXPECT().ExecWithWriter(ctx, "cmd", gomock.Any(), "-LOCK", db).Return(errWant)
+	_, err := manager.Lock(ctx, db, false)
+	assert.EqualError(t, err, errWant.Error())
+
+	executer.EXPECT().ExecWithWriter(ctx, "cmd", gomock.Any(), "-LOCK", db)
+	got, err := manager.Lock(ctx, db, false)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(0), got)
+
+	executer.EXPECT().ExecWithWriter(ctx, "cmd", gomock.Any(), "-SIZE", "-LOCK", db).DoAndReturn(
+		func(ctx context.Context, commandLine string, w io.Writer, args ...string) error {
+			w.Write([]byte(`abc`))
+			return nil
+		})
+	_, err = manager.Lock(ctx, db, true)
+	assert.EqualError(t, err, `strconv.Atoi: parsing "abc": invalid syntax`)
 }
 
 func TestManager_Version(t *testing.T) {
